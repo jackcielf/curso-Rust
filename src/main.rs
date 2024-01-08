c876d6d88554754419edeea46d42b1f305595761
JakkiFx <jackcielfelix555@gmail.com>
2024-01-08 14:46:45 -0300

    Diferenca entre let e const, duas contantes

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+
+    const NAME: &str = "Jack"; // Necessario explicitar o tipo
+    
+    let mut name = "Jack"; // Je entende qual o tipo
+    
+    println!("Valor da constante é: {}", NAME);
+    println!("Valor da variável é: {}", name);
+}
